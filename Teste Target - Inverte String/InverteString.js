a3ba031e1d7da2cba352aca11da0a126261c71b0
Lukas ( Pedro Lucas ) <plucask337@gmail.com>
2023-04-30 19:18:11 -0300

    Teste_Target - Fibonaccio/InvertedString

---
+++ Teste Target - Inverte String/InverteString.js
@@ -0,0 +1,18 @@
+function inverterString (string) {
+
+  let strReverseDefinitiva = string.split('').reverse('').join('')
+
+  // split - transforma uma palavra em array
+
+  // reverse - inverte os elementos de um array
+
+  // Join - transforma um array em string.
+
+  console.log(strReverseDefinitiva)
+
+  //use f12 para ver o resultado.
+
+}
+ 
+inverterString("Pedro Lucas")
+
